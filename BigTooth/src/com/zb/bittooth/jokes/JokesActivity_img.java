62d397bc9a3d81948556d939f7bc7363b5c2f090
a4611764a <954884146@qq.com>
2015-08-18 20:09:53 +0800

    1

--- BigTooth/src/com/zb/bittooth/jokes/JokesActivity_img.java
+++ BigTooth/src/com/zb/bittooth/jokes/JokesActivity_img.java
@@ -15,7 +15,6 @@ import com.zb.bittooth.customView.CustomProgressDialog;
 import com.zb.bittooth.utils.T;
 
 public class JokesActivity_img extends Activity {
-	//gson
 	Gson gson = new Gson();
 	private HttpUtils httpUtils = null;
 	CustomProgressDialog dialog;
